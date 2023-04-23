940b4021ff3dc03675f98da2b6087f71cbfbf9b9
Larry Fang <lfang615@gmail.com>
2023-04-23 18:22:49 -0500

    Add Position pydantic model to ordermanager

---
+++ ordermanager/app/models/position.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional
+from app.models.order import Order
+
+class Position(BaseModel):
+    symbol: str
+    side: str
+    openDate: str
+    qty: int
+    aep: float
+    linkedOrders: list[Order]
+    closeDate: Optional[str] = None
