a46b3856a4731056849b3e2041a5ee1f1e5a9c1d
Larry Fang <lfang615@gmail.com>
2023-04-24 13:46:16 -0500

    Add mongodb as related service for user credentials

--- ordermanager/app/main.py
+++ ordermanager/app/main.py
@@ -1,15 +1,18 @@
-from fastapi import FastAPI, WebSocket, Request
+from fastapi import FastAPI, WebSocket, Request, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.templating import Jinja2Templates
 from starlette.websockets import WebSocketDisconnect
 from fastapi.responses import HTMLResponse
+from fastapi.exceptions import HTTPException
 from bson.json_util import dumps
 from ordermanager.app.bybit import BybitAPI, BybitAPIException
-from ordermanager.app.models.request.order import OrderRequest, CancelRequest, LeverageRequest
+from ordermanager.app.security import *
+from ordermanager.app.models.order import *
 from aiokafka import AIOKafkaProducer, AIOKafkaConsumer
 from asyncio import Queue
-from typing import Optional
+from typing import Annotated, Optional
 from pydantic import BaseModel
 import os
 from dotenv import load_dotenv
@@ -19,10 +22,12 @@ import asyncio
 import json
 import logging
 import uuid
-from typing import List
+from jose import JWTError, jwt
 
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 # Configure the static files middleware to serve files from the `/static` folder
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -48,6 +53,8 @@ app.add_middleware(
 load_dotenv()
 
 api = BybitAPI(api_key=os.environ['BYBIT_API_KEY'], secret_key=os.environ['BYBIT_API_SECRET'])
+security = Security()
+
 
 def generate_unique_id():
     return str(uuid.uuid4())
@@ -61,6 +68,15 @@ async def serve_vue_app(request: Request):
     except Exception as e:
         logging.error("Error serve_vue_app, {e}")
         return HTMLResponse(status_code=404)
+    
+@app.get("/risk_limits/{symbol}")
+async def get_risk_limits(symbol:str):
+    try:
+        response = await api.MarketData(api).get_risk_limits(symbol)
+        return response
+    except (BybitAPIException, HTTPException) as e:
+        logging.error("Error get_risk_limits, {e}")
+        return {"error": str(e)}
 
 @app.get("/order/")
 async def get_order(order_id: Optional[str]=None, symbol: Optional[str]=None):
@@ -72,7 +88,7 @@ async def get_order(order_id: Optional[str]=None, symbol: Optional[str]=None):
         else:
             return {"error": "You must provide either an orderId or a symbol"}
         return response
-    except (BybitAPIException, Exception) as e:
+    except (BybitAPIException, HTTPException) as e:
         logging.error("Error get_order, {e}")
         return {"error": str(e)}
 
@@ -114,15 +130,13 @@ async def place_order(orderRequest: OrderRequest):
         
         response = await api.ContractOrder(api).place_order(orderRequest)
         
-        # Create a new order object in "Orders" cache
-        order_data = orderRequest.dict()
-        orderRequest['orderStatus'] = "Pending"   
-        app.state.redis_client.hset("orders", orderRequest['orderLinkId'], json.dumps(order_data))
-
         if response['retCode'] == 0:
-            await produce_topic_orders_executed(app, response['result'])
-        # Publish order ID to order_updates channel
-        # app.state.redis_client.publish("order_updates", order_placed['result']['orderId'])
+            orderRequest['orderId'] = response['result']['orderId']
+            orderRequest['orderStatus'] = "Pending"
+            order_data = orderRequest.dict()
+            order_data = Order(**order_data)       
+            await produce_topic_orders_executed(app, order_data)
+        
         return response
     except (BybitAPIException, Exception) as e:
         logging.error("Error place_order, {e}")
@@ -144,6 +158,59 @@ async def set_leverage(leverageRequest: LeverageRequest):
     except (BybitAPIException, Exception) as e:
         logging.error("Error set_leverage, {e}")
         return {"error": str(e)}
+    
+@app.post("/set_wallet_balance")
+async def set_wallet_balance(walletBalanceRequest: WalletBalanceRequest):
+    try:
+        wallet_balance_key = "wallet_balance"
+        wallet_balance_data = app.state.redis_client.get(wallet_balance_key)
+        
+        wallet_balance = WalletBalance(**json.loads(wallet_balance_data))
+        wallet_balance['walletBalance'] = walletBalanceRequest['wallet_balance']
+        app.state.redis_client.set(wallet_balance_key, json.dumps(wallet_balance.dict()))
+        
+    except (HTTPException) as e:
+        logging.error("Error set_wallet_balance, {e}")
+        return {"error": str(e)}
+    
+# ----------------------------- Authentication ---------------------------------
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = security.authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=os.environ['ACCESS_TOKEN_EXPIRE_MINUTES'])
+    access_token = security.create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        security = Security()
+        payload = jwt.decode(token, os.environ['SECRET_KEY'], algorithms=[os.environ['ALGORITHM']])
+        username: str = payload.get("sub")
+        if username is None:
+            raise 
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = security.get_user(token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
 
 # ----------------------------- WEBSOCKET Connections for frontend updates  ---------------------------------
 @app.websocket("/ws/orders")
@@ -182,7 +249,7 @@ async def send_positions_update(position):
 
 # --------------------------- BACKGROUND TASKS -------------------------------
 
-async def produce_topic_orders_executed(app, order):
+async def produce_topic_orders_executed(app, order: Order):
     try:
         producer = app.state.kafka_producer
         message = json.dumps(order)
--- ordermanager/app/security.py
+++ ordermanager/app/security.py
@@ -6,6 +6,20 @@ from pydantic import BaseModel
 import os
 from pymongo import MongoClient
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+class User(BaseModel):
+    username: str
+
+class UserInDB(User):
+    hashed_password: str
 
 MONGO_URL = os.environ["MONGO_URL"]
 client = MongoClient(MONGO_URL)
@@ -13,34 +27,46 @@ client = MongoClient(MONGO_URL)
 db = client["ordermanager"]
 users_collection = db["users"]
 
+class Security:
+    oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-SECRET_KEY = "your_secret_key" # Generate a strong secret key
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+    SECRET_KEY = os.environ['SECRET_KEY'] # Generate a strong secret key
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 43200 # 30 days
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+    def verify_password(self, plain_password, hashed_password):
+        return self.pwd_context.verify(plain_password, hashed_password)
 
-def get_password_hash(password):
-    return pwd_context.hash(password)
+    def get_password_hash(self, password):
+        return self.pwd_context.hash(password)
 
-def create_access_token(data: dict, expires_delta: timedelta = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+    def create_access_token(self, data: dict, expires_delta: timedelta = None):
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.utcnow() + expires_delta
+        else:
+            expire = datetime.utcnow() + timedelta(minutes=15)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, os.environ['SECRET_KEY'], algorithm=os.environ['ALGORITHM'])
+        return encoded_jwt
 
-def authenticate_user(username: str, password: str):
-    user = ... # Retrieve user from MongoDB using the provided username
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
+    def authenticate_user(self, username: str, password: str):
+        user = users_collection.find_one({"username": username, "hashed_password": self.get_password_hash(password)})
+        if not user:
+            return False
+        if not self.verify_password(password, user.hashed_password):
+            return False
+        user = UserInDB(**user)
+        return user
+    
+    def decode_payload(self, token: str):
+        payload = jwt.decode(token, os.environ['SECRET_KEY'], algorithms=[os.environ['ALGORITHM']])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+        token_data = TokenData(username=username)
+        return token_data
     
